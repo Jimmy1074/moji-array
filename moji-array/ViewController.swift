a379aa50ba9f4288b2a4caa469b6f8b0c883ffcd
jasonfan <staind1074@gmail.com>
2016-11-04 21:38:34 +0800

    Add tableview and 2st screen

--- moji-array/ViewController.swift
+++ moji-array/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var Mojitableview: UITableView!
+    
+    var emojis = ["💩", "😝", "😡", "🤔", "😻"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Mojitableview.dataSource = self;
+        Mojitableview.delegate = self;
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "emojicell", for: indexPath)
+        cell.textLabel?.text = emojis[indexPath.row];
+        return cell;
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
